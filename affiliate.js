c3d228390c4667ed9ddf233c222e219a96b9c6aa
adunolawal <99498748+adunolawal@users.noreply.github.com>
2022-07-23 13:00:09 -0700

    changes

--- affiliate.js
+++ affiliate.js
@@ -16,28 +16,3 @@ for(i=0; i < acc.length; i++){
     })
 }
 
-// modal popup
-let modal = document.querySelector('.modal')
-let popbtn = document.querySelector('.seehowBtn')
-let closebtn = document.querySelector('.close')
-let video = document.querySelector('.video')
-
-popbtn.addEventListener('click', ()=>{
-    modal.classList.add('activate')
-    video.setAttribute("allow", "autoplay");
-    video.play()
-    video.currentTime = 0;
-})
-closebtn.addEventListener('click', ()=>{
-    modal.classList.remove('activate')
-    video.removeAttribute("allow", "autoplay")
-    // video.src = " ";
-    video.pause();
-})
-
-// closebtn.onclick = function(){
-//     video.removeAttribute("allow", "autoplay")
-//     modal.style.display = "none"
-//     video.pause();
-//     video.currentTime = 0;
-// }
