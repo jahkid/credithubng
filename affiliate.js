a33498fde8f8c4be940e52837a2dbc16a4ea8486
adunolawal <99498748+adunolawal@users.noreply.github.com>
2022-07-23 09:33:31 -0700

    added affliate files

--- affiliate.js
+++ affiliate.js
@@ -1,3 +1,4 @@
+// FAQs
 var acc = document.getElementsByClassName("questions");
 var i;
 
@@ -14,3 +15,29 @@ for(i=0; i < acc.length; i++){
     }
     })
 }
+
+// modal popup
+let modal = document.querySelector('.modal')
+let popbtn = document.querySelector('.seehowBtn')
+let closebtn = document.querySelector('.close')
+let video = document.querySelector('.video')
+
+popbtn.addEventListener('click', ()=>{
+    modal.classList.add('activate')
+    video.setAttribute("allow", "autoplay");
+    video.play()
+    video.currentTime = 0;
+})
+closebtn.addEventListener('click', ()=>{
+    modal.classList.remove('activate')
+    video.removeAttribute("allow", "autoplay")
+    // video.src = " ";
+    video.pause();
+})
+
+// closebtn.onclick = function(){
+//     video.removeAttribute("allow", "autoplay")
+//     modal.style.display = "none"
+//     video.pause();
+//     video.currentTime = 0;
+// }
