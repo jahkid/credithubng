d57bfd97b4848679e9c67e74de89ad7c68bcef1e
jahkid <31428752+jahkid@users.noreply.github.com>
2022-07-26 21:17:06 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
-<?php require dirname(__DIR__).'/credithubng/controllers/authController.php'; 
+<?php require 'controllers/authController.php'; 
  ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+  <base href="https://credithub.herokuapp.com">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
