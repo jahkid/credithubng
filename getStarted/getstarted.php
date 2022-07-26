465fe188c971b2d581b4c2daf939a99afc9fa6a4
jahkid <jahkidedeh@gmail.com>
2022-07-27 00:30:03 +0100

    Update getstarted.php

--- getStarted/getstarted.php
+++ getStarted/getstarted.php
@@ -1,102 +1,120 @@
-<?php require '../controllers/authController.php'; ?>
-
+<?php require dirname(__DIR__).'./controllers/authController.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-  <base href="https://credithub.herokuapp.com/getStarted/">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-    <!-- <link rel="stylesheet" href="style.css" /> -->
-    <link rel="stylesheet" href="getstarted.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="getstarted.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600&display=swap" rel="stylesheet">
     <title>CreditHub</title>
   </head>
   <body>
-    <div class="container-wrapper">
-      <header class="app-bar">
-        <img class="image" src="images/Logos.png" alt="logo" />
-        <button onclick="openMenu()" id="menu-icon">
-          <img src="images/Menu.png" alt="menu" />
+    <div class="container">
+      <!-- this is the first section logo, hamburger  -->
+      <div class="topbar">
+        <span
+          ><img src="images/Logos.png" alt="logo" />
+        </span>
+        <button class="menu-btn" id="menu-btn" onclick="openModal()"> 
+          <img src="images/Menu.png" alt="" />
         </button>
-        <div id="menu" class="menu">
-          <button onclick="cancel()" id="cancel">&times;</button>
-          <ul>
-            <a href="../index.html"><li>Home</li></a>
-            <a href="../affiliate.html"><li>Affliates</li></a>
-            <a href="#"><li>Contact Us</li></a>
-            <a href="#"><li>FAQ</li></a>
-            <a id="getstarted" href="#">For Business</a>
-            <a id="login" href="#">Login</a>
-          </ul>
+      </div>
+      <!-- modal menu  -->
+      <div id="menuContainer" class="menu-container">
+        <div class="menu-header">
+            <span class="logo-img"><img src="images/Logos.png" alt="logo" /></span>
+            <button class="logo-close" id="close-btn" onclick="closeModal()"> 
+                <img src="images/Close_round.png" alt="">
+            </button>
         </div>
-        <div id="menus" class="menus">
-            <button onclick="cancel()" id="cancel">&times;</button>
+        <div class="menu" id="menu">
             <ul>
-              <a href="../index.html"><li>Home</li></a>
-              <a href="../affiliate.html"><li>Affliates</li></a>
-              <a href="#"><li>Contact Us</li></a>
-              <a href="#"><li>FAQ</li></a>
-              <a id="getstarted" href="#">For Business</a>
-              <a id="login" href="../login.php">Login</a>
+               <span class="content">
+                   <li>For Business</li>
+                   <li>For Affiliates</li>
+                   <li>Contact Us</li>
+                   <li>FAQ</li>
             </ul>
-          </div>
-      </header>
-      <main>
-          <div class="form-title">
-              <h1>Get your company started on CreditHub</h1>
-          </div>
-        <div class="input-form">
-            <div class="col-1">
-                <div class="input-field">
-                    <label for="fullname">Full Name</label>
-                    <input type="text" name="fullname" placeholder="Enter your name" id="fullname" required>
-                </div>
-                <div class="input-field">
-                    <label for="email">Email Address</label>
-                    <input type="email" name="email" placeholder="youremail@email.com" id="email" required>
-                </div>
-                <div class="input-field">
-                    <label for="location">Location</label>
-                    <input type="text" name="location" id="location" required>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="input-field">
-                    <label for="company">Company Name</label>
-                    <input type="text" name="company" placeholder="Enter your comapny name" id="company" required>
-                </div>
-                <div class="input-field">
-                    <label for="phone">Phone Number</label>
-                    <input type="text" name="phone" id="phone" placeholder="e.g 08012345678" required>
-                </div>
-                <div class="input-field">
-                    <label for="product">Product or Service Name</label>
-                    <input type="text" name="product" id="product" required>
-                </div>
-            </div>
+           
         </div>
-        <div class="field">
-            <label for="desc">Brief Product description</label>
-            <textarea name="desc" id="desc" cols="30" rows="10" style="height: 210px;"></textarea>
-        </div>
-        <div class="submit">
-            <input type="submit" value="Submit">
-        </div>
-      </main>
-    </div>
-    <script>
-      const openMenu = () => {
-        document.getElementById("menus").style.display = "block";
-      };
+        <div class="btoon">
+          <span class="get-btn">
+            <input type="submit" placeholder="Get Started" value="Get Started">
+          </span>
+          <span class="log-button">
+            <input type="submit" name="Login" value="Login">
+          </span>
+      </div>
+      </div>
+      
+      
 
-      const cancel = () => {
-        document.getElementById("menus").style.display = "none";
-      }
-    </script>
+      <!-- this is the content l6part -->
+      <div class="form-container">
+        <div class="content">
+          <h1>Get your company started on CreditHub</h1>
+        </div>
+        <!-- this section entails the input  -->
+        
+        <form enctype = 'multipart/form-data' action="https://getform.io/f/f111d183-7ab1-4dac-a0d9-2574d122a651" method="post">
+        <?php if (count($errors)>0):?>
+        <div class="alert alert-danger">
+          <?php foreach($errors as $error): ?>
+          <li><?php echo $error; ?></li>
+          <?php endforeach; ?>
+        </div>
+        <?php endif; ?>
+        <div class="input">
+          <label for="full-name">Full Name </label>
+          <input type="text" placeholder="Enter your name" name = 'fullname'required />
+          <label for="company">Company Name </label>
+          <input type="text" placeholder="Enter your company's name" name="company" required />
+          <label for="email">Email Address </label>
+          <input type="email" placeholder="youremail@email.com" name="email" required />
+          <label for="password">Password </label>
+          <input type="password" placeholder=" " name="password" required />
+          <label for="passwordConf">Confirm Password </label>
+          <input type="password" placeholder=" " name="passwordConf" required />
+          <label for="phone">Phone Number</label>
+          <input type="number" placeholder="e.g 08012345678" name="phone" required />
+          <label for="location">Location</label>
+          <input type="text" name="location" />
+          <label for="product">Product or Service Name</label>
+          <input type="text" name="product" required />
+          <label for="text"
+            >Brief Product Description</label
+          >
+          <input
+            name = 'product'
+            type="text"
+            class="brief"
+            placeholder="Tell us about the product or service"
+            required>
+          
+          
+              <span
+                ></span>
+              <div class="upload-container">
+                <label for="file">Upload Document (kindly upload document to help us understand
+                  your business better)</label>
+                <input
+                name="file"
+                type="file" placeholder= "Select files here" 
+                value="Supported file types: JPEG, PNG, PDF Max File size: 2mb"
+                required
+                />
+              </div>
+          </div>
+          <button class="btn" name="signup-btn">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <script src="getstarted.js"></script>
   </body>
 </html>
