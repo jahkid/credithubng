17344c5b6cff7a8bb35032a60c6ba2838df9edf9
jahkid <31428752+jahkid@users.noreply.github.com>
2022-07-26 20:13:08 +0100

    Update getstarted.php

--- getStarted/getstarted.php
+++ getStarted/getstarted.php
@@ -1,13 +1,15 @@
-<?php //require dirname(__DIR__).'./controllers/authController.php';
+<?php //require dirname(__DIR__).'../controllers/authController.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+   <base href="/oguntimehinilemibayo/app/">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-    <link rel="stylesheet" href="style.css" />
+    <!-- <link rel="stylesheet" href="style.css" /> -->
+    <link rel="stylesheet" href="getstarted.css.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
