1a5d8681d002194ed9d9c504396bcfa4a7fb267b
jahkid <31428752+jahkid@users.noreply.github.com>
2022-07-26 20:14:14 +0100

    Update getstarted.php

--- getStarted/getstarted.php
+++ getStarted/getstarted.php
@@ -1,4 +1,4 @@
-<?php //require dirname(__DIR__).'../controllers/authController.php';
+<?php require dirname(__DIR__).'../controllers/authController.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
     <!-- <link rel="stylesheet" href="style.css" /> -->
-    <link rel="stylesheet" href="getstarted.css.css" />
+    <link rel="stylesheet" href="getstarted.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
