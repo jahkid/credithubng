0f3308bba149570ac7bd67f58f967538facf19aa
jahkid <31428752+jahkid@users.noreply.github.com>
2022-07-26 21:03:53 +0100

    Update getstarted.php

--- getStarted/getstarted.php
+++ getStarted/getstarted.php
@@ -1,11 +1,10 @@
-<?php require '../controllers/authController.php';
-//include_once '../controllers/authController.php';
-echo 's';
-?>
+<?php require '../controllers/authController.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+  <base href="http://credithub.herokuapp.com/getStarted/">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
