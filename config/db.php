a1817dfb104d80c21a71ee885a17ad35a610a024
jahkid <31428752+jahkid@users.noreply.github.com>
2022-07-26 08:58:18 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,21 @@
 <?php
 require 'constants.php';
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if ($conn->connect_error) {
-   die('Database error:'.$conn->connect_error);
-}
+// if ($conn->connect_error) {
+//    die('Database error:'.$conn->connect_error);
+// }
+
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
